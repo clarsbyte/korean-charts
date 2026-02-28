c4d3c321eb5ff74746f4aeb28d30136ce1442066
Dirb <clarissa.aurelia1001@gmail.com>
2026-02-28 10:33:03 -0800

    Added logo

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const { scrapeYouTubeVideo } = require("../../../index.js");
+const { scrapeYouTubeVideo } = require("../index.js");
 
 export const runtime = "nodejs";
 
